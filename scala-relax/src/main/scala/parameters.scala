a501bbaedb94b67076a36a02085cbab05f93a42e
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-07-05 06:11:27 -0400

    added more comments, created particle class

--- scala-relax/src/main/scala/parameters.scala
+++ scala-relax/src/main/scala/parameters.scala
@@ -2,4 +2,11 @@ package parameters
 
 class Parameters {
   var projectile: String = "H"
+
+  def read_parameters(filename: String): Parameters = {
+    val par = new Parameters
+    par.projectile = "He"
+    par
+  }
+
 }
