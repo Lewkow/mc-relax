426bcac9f8551e531321ad61fa2dfb8dd6f7cf93
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-07-31 20:02:36 -0400

    added HotParticle class

---
+++ scala-relax/src/main/scala/hotParticle.scala
@@ -0,0 +1,30 @@
+package hotParticle
+
+class HotParticle() 
+
+  var projectileName: String = ""
+  var projecileMass: Double = 0.0
+  var currentPosition: (Double, Double, Double) = (0.0, 0.0, 0.0)
+  var currentVelocity: (Double, Double, Double) = (0.0, 0.0, 0.0)
+  var currentEnergy: Double = 0.0
+  var currentTime: Double = 0.0
+  var numberOfCollisions: Int = 0
+  var numberOfClicks: Int = 0
+
+  def setParameters(name: String, 
+                    mass: Double, 
+                    initPosition: (Double, Double, Double),
+                    intitVelocity: (Double, Double, Double)) {
+    projectileName = name
+    projecileMass = mass
+    currentPosition = initPosition
+    currentVelocity = intitVelocity
+  }
+
+  def getEnergy: Double = {
+    math.sqrt(currentVelocity._1*currentVelocity._1 +
+              currentVelocity._2*currentVelocity._2 + 
+              currentVelocity._3*currentVelocity._3)*0.5d*projecileMass
+  }
+  
+}
