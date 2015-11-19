9d81683db3646135b54b7da6fb807627e2b15163
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-11-19 13:25:02 -0500

    implemented universal amp and tcs

--- scala-relax/src/main/scala/crossSections.scala
+++ scala-relax/src/main/scala/crossSections.scala
@@ -7,6 +7,11 @@ import math._
 
 class CrossSections extends Serializable {
 
+  val use_Universal: Boolean = false
+
+  // number of angles for numeric integrations
+  val N_angles: Int = 10000
+
   // random object generator
   var randy: Random = new Random()
 
@@ -56,14 +61,35 @@ class CrossSections extends Serializable {
   }
 
   // universal scattering random angle
+  // Inputs: cm_energy [eV], target species
+  // Outputs: cm_scattering_angle [deg]
   def universalScatteringAngle(energy: Double, target: String): Double = {
-    universalTotalCrossSection(energy, target)
+    val tcs: Double = universalTotalCrossSection(energy, target)
+    val da: Double = math.Pi*2.0/N_angles.toDouble  
+    var rho: Double = 0.0
+    var r: Double = randy.nextDouble()
+    var gotIt: Boolean = false
+    var i: Int = 1
+    var scattering_theta: Double = 0.0
+
+    while (rho < r) {
+      scattering_theta = toDegrees(da*i.toDouble)
+      i += 1
+      rho += (da*2.0*math.Pi*math.sin(toRadians(scattering_theta))/tcs)*universalAmplitude(energy, scattering_theta, target)
+    }
+    scattering_theta
   }
 
   def universalTotalCrossSection(energy: Double, target: String): Double = {
-    val N_angles: Double = 10000
-    val da: Double = math.Pi*2.0/N_angles  
-    da   
+    // differential angle [rad] 
+    val da: Double = math.Pi*2.0/N_angles.toDouble
+    var tcs: Double = 0.0
+    var i: Int = 1
+    for (i <- 1 until N_angles-1) {
+      val theta: Double = toDegrees(da*i)
+      tcs += da*2.0*math.Pi*math.sin(toRadians(theta))*universalAmplitude(energy, theta, target)
+    }
+    tcs
   }
 
   // constructor for object
@@ -82,7 +108,11 @@ class CrossSections extends Serializable {
   // lab-frame energy [eV]
   // returns -> Random scattering angle [deg]
   def getScatteringAngle(energy: Double, target: String): Double = {
-    gaussianScatteringAngle(10.0d) 
+    if (use_Universal) {
+      universalScatteringAngle(energy, target)
+    } else {
+      gaussianScatteringAngle(10.0d) 
+    }
   }
 
   def gaussianScatteringAngle(mean: Double): Double = { randy.nextGaussian() + mean }
@@ -91,8 +121,16 @@ class CrossSections extends Serializable {
   // lab-frame energy [eV]
   // returns -> Total cross section [m^2]
   def getTotalCrossSection(energy: Double, target: String): Double = {
-    1.0e-15
+    if (use_Universal) {
+      universalTotalCrossSection(energy, target)
+    } else {
+      1.0e-15
+    }
   }
+
+  def toDegrees(rad: Double): Double = { rad*360.0d/(2.0d*math.Pi) }
+
+  def toRadians(deg: Double): Double = { deg*2.0d*math.Pi/360.0d }
 
 }
 
